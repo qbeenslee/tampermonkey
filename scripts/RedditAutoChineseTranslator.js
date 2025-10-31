ee7f761b96e63de96010f432021362447532f19c
qbeenslee <qbeenslee@gmail.com>
2025-10-31 22:17:15 +0800

    feat(translator): add more Chinese subreddits to exclusion list

--- scripts/RedditAutoChineseTranslator.js
+++ scripts/RedditAutoChineseTranslator.js
@@ -34,7 +34,12 @@
 		// 已是中文的subreddit列表（无需翻译）
 		CHINESE_SUBREDDITS: [
 			'China_irl',
-			'weibo_read'
+			'weibo_read',
+			'USstocks2discuss',
+			'dashuju',
+			'Chinatown_irl',
+			'taiwanica',
+			'China_teahouse'
 			// 可以继续添加其他中文subreddit
 		],
 	};
